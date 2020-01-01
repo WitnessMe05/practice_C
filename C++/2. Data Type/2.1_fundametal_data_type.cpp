fa6bc5ed3c2113a4ec9fe3902aeace4a9ae484b1
gnlenfn <gnlenfn@gmail.com>
2020-01-01 09:43:41 +0900

    12.31

--- C++/2. Data Type/2.1_fundametal_data_type.cpp
+++ C++/2. Data Type/2.1_fundametal_data_type.cpp
@@ -15,13 +15,21 @@ int main() {
     auto aValue = 3.141592;
     auto aValue2 = 3.141592f;
 
-    cout << (bValue ? 1 : 0) << endl;
-    cout << (int)chValue << endl; // char과 bool은 내부적으로 int로 저장한다
-    cout << fValue << endl;
-    cout << dValue << endl;
-    cout << "" << endl;
-    printf("\n");
-    cout << sizeof(aValue) << endl;
-    cout << sizeof(aValue2) << endl;
+    // cout << (bValue ? 1 : 0) << endl;
+    // cout << (int)chValue << endl; // char과 bool은 내부적으로 int로 저장한다
+    // cout << fValue << endl;
+    // cout << dValue << endl;
+    // cout << "" << endl;
+    // cout << sizeof(aValue) << endl;
+    // cout << sizeof(aValue2) << endl;
+
+    // Initialization
+    int a(123); // direct initialization
+    int b{ 123 }; // uniform initialization
+    int a = 123; // copy initialization
+    // copy와 direct는 맞지 않는 자료형이 들어와도 맞게 바꿔서 저장함, uniform에서는 더 엄격하게 적용해서 error 발생
+
+    int k, l; // 두 개 이상 동시에 선언, 같은 type 끼리만 가능
+    int x = 10, y(11), z{12};
     return 0;
 }
