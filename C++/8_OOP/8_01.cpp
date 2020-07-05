7d2d9887e3823a754b6924dcf6b33c45868d53c4
Yun-Hong-In <gnlenfn@gmail.com>
2020-07-05 22:38:14 +0900

    markdown

---
+++ C++/8_OOP/8_01.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Friend
+{
+public: // access specifier (public, private, protected 3 가지 있음)
+    string name_;
+    string m_address;
+    int    _age;
+    double height;
+    double weight;
+
+    void print()
+    {
+        cout << name_ << " " << m_address << " " << _age << " " << height << " " << weight << endl;
+    }
+};
+
+
+int main()
+{
+    Friend jj{"Jack Jack", "Uptown", 2, 30, 10};
+    jj.print();
+
+
+    vector<Friend> my_friends;
+    my_friends.resize(2);
+
+    for(auto &e : my_friends)
+        e.print()
+}
